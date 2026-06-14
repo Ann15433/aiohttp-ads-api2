2f5daf322357e08ae192597c820a1a8d586a1c84
Ann15433 <manya.lukyanchikova@yandex.com>
2026-06-14 23:29:33 +0300

    middleware.py

--- middleware.py
+++ middleware.py
@@ -3,14 +3,12 @@ from models import decode_jwt_token
 
 async def auth_middleware(app, handler):
     async def middleware_handler(request):
-        # ПРОПУСКАЕМ ПУБЛИЧНЫЕ МАРШРУТЫ В САМОМ НАЧАЛЕ
         if (
             request.path in ['/', '/register', '/login']
             or (request.path.startswith('/ads/') and request.method == 'GET')
         ):
             return await handler(request)
 
-        # Теперь проверяем авторизацию для остальных маршрутов
         auth_header = request.headers.get('Authorization')
         if not auth_header or not auth_header.startswith('Bearer '):
             return web.json_response(
@@ -18,7 +16,7 @@ async def auth_middleware(app, handler):
                 status=401
             )
 
-        token = auth_header[7:]  # убираем 'Bearer '
+        token = auth_header[7:]  
 
         try:
             payload = decode_jwt_token(token)
